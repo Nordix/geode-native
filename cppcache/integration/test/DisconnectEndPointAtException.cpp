e8705356c043be0263c812024af22a5f511ddd13
jvarenina <jakov.varenina@est.tech>
2020-12-03 07:57:00 +0100

    Working

--- cppcache/integration/test/DisconnectEndPointAtException.cpp
+++ cppcache/integration/test/DisconnectEndPointAtException.cpp
@@ -18,6 +18,7 @@
 #include <framework/Gfsh.h>
 #include <framework/TestConfig.h>
 
+#include <chrono>
 #include <thread>
 
 #include <geode/Cache.hpp>
@@ -49,16 +50,6 @@ std::string getClientLogName() {
   return logFileName;
 }
 
-void removeLogFromPreviousExecution() {
-  std::string logFileName(getClientLogName());
-  std::ifstream previousTestLog(logFileName);
-  if (previousTestLog.good()) {
-    std::cout << "Removing log from previous execution: " << logFileName
-              << std::endl;
-    remove(logFileName.c_str());
-  }
-}
-
 std::shared_ptr<Cache> createCache() {
   auto cache = CacheFactory()
                    .set("log-level", "debug")
@@ -66,130 +57,111 @@ std::shared_ptr<Cache> createCache() {
                    .create();
   return std::make_shared<Cache>(std::move(cache));
 }
-
-std::shared_ptr<Pool> createPool(Cluster& cluster, std::shared_ptr<Cache> cache,
-                                 bool useSingleHop) {
-  auto poolFactory = cache->getPoolManager().createFactory();
-  cluster.applyLocators(poolFactory);
-  poolFactory.setPRSingleHopEnabled(useSingleHop);
-  return poolFactory.create("default");
-}
-
-std::string addKeyPrefix(int key) {
-  return std::to_string(key) + "|" + std::to_string(key);
-}
-
+/*
 void runClientOperations(std::shared_ptr<Cache> cache,
-                         std::shared_ptr<Region> region, int minEntryKey,
-                         int maxEntryKey, int numTx,
-                         bool usingPartitionResolver) {
+                         std::shared_ptr<Region> region) {
+  auto transactionManager = cache->getCacheTransactionManager();
+  int theKey = 7;
+  std::string theValue = "theValue";
+  try {
+    transactionManager->begin();
+    region->put(theKey, theValue);
+    transactionManager->commit();
+  } catch (...) {
+    if (transactionManager->exists()) {
+      transactionManager->rollback();
+    }
+  }
+
+  EXPECT_TRUE(region->containsKey(7));
+}
+*/
+void executeTestCaseOneTransaction() {
+  Cluster cluster{
+      LocatorCount{1}, ServerCount{3},
+      CacheXMLFiles({
+          std::string(getFrameworkString(FrameworkVariable::TestCacheXmlDir)) +
+              "/cacheserver1_fpr_transaction.xml",
+          std::string(getFrameworkString(FrameworkVariable::TestCacheXmlDir)) +
+              "/cacheserver2_fpr_transaction.xml",
+          std::string(getFrameworkString(FrameworkVariable::TestCacheXmlDir)) +
+              "/cacheserver3_fpr_transaction.xml",
+      })};
+  cluster.start([&]() {
+    cluster.getGfsh()
+        .deploy()
+        .jar(getFrameworkString(FrameworkVariable::JavaObjectJarPath))
+        .execute();
+  });
+
+  auto cache = createCache();
+  auto poolFactory = cache->getPoolManager().createFactory();
+  ServerAddress serverAddress = cluster.getServers()[0].getAddress();
+  serverAddress.port = 40401;
+  cluster.applyServer(poolFactory, serverAddress);
+  auto pool = poolFactory.create("default");
+  auto region = cache->createRegionFactory(RegionShortcut::PROXY)
+                    .setPoolName("default")
+                    .create("region");
+
+  // std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+  // std::thread th(runClientOperations, cache, region);
+
   auto transactionManager = cache->getCacheTransactionManager();
 
-  for (int i = 0; i < numTx; i++) {
-    auto theKey = (rand() % (maxEntryKey - minEntryKey)) + minEntryKey;
-    std::string theValue = "theValue";
+  // this key will be always routed towards server[1]
+  int theKey = 7;
+  std::string theValue = "theValue";
+  try {
+    transactionManager->begin();
+    region->put(theKey, theValue);
+    transactionManager->commit();
+  } catch (...) {
+    if (transactionManager->exists()) {
+      transactionManager->rollback();
+    }
+  }
+
+  try {
+    transactionManager->begin();
+    region->put(theKey, theValue);
+    transactionManager->commit();
+  } catch (...) {
+    if (transactionManager->exists()) {
+      transactionManager->rollback();
+    }
+  }
+
+  EXPECT_TRUE(region->containsKey(7));
+  EXPECT_FALSE(region->containsKey(7));
+
+  // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  // cluster.getServers()[1].stop();
+
+  // std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+
+  /*
+  try {
+    transactionManager->commit();
+  } catch (...) {
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+
+  // cluster.getServers()[1].stop();
+
     try {
-      transactionManager->begin();
-      if (usingPartitionResolver) {
-        region->put(addKeyPrefix(theKey), theValue);
-      } else {
-        region->put(theKey, theValue);
-      }
       transactionManager->commit();
     } catch (...) {
       if (transactionManager->exists()) {
         transactionManager->rollback();
       }
     }
-  }
-}
+  */
+}  // executeTestCaseOneTransaction
 
-void checkAllDisconnectionsAreFromSameServer(
-    std::shared_ptr<std::vector<std::string>> appearances) {
-  if (appearances->size() <= 1) {
-    return;
-  }
-  std::string firstServerDisconnected("");
-  for (std::string line : *appearances) {
-    auto index = line.find(disconnectingStr);
-    std::string serverDisconnected =
-        line.substr(index + disconnectingStr.length());
-    if (firstServerDisconnected == "") {
-      firstServerDisconnected = serverDisconnected;
-    } else {
-      ASSERT_EQ(firstServerDisconnected, serverDisconnected);
-    }
-  }
-}
-
-void executeTestCase(bool useSingleHopAndPR) {
-  int NUM_THREADS = 8;
-  int MAX_ENTRY_KEY = 100000;
-  int TX_PER_CLIENT = 1000;
-  auto keyRangeSize = (MAX_ENTRY_KEY / NUM_THREADS);
-
-  Cluster cluster{LocatorCount{1}, ServerCount{2}};
-  cluster.start();
-  auto region_cmd =
-      cluster.getGfsh().create().region().withName("region").withType(
-          "PARTITION");
-  if (useSingleHopAndPR) {
-    region_cmd
-        .withPartitionResolver(
-            "org.apache.geode.cache.util.StringPrefixPartitionResolver")
-        .execute();
-  } else {
-    region_cmd.execute();
-  }
-
-  auto cache = createCache();
-  auto pool = createPool(cluster, cache, useSingleHopAndPR);
-  auto region = cache->createRegionFactory(RegionShortcut::PROXY)
-                    .setPoolName("default")
-                    .create("region");
-
-  std::vector<std::thread> clientThreads;
-  for (int i = 0; i < NUM_THREADS; i++) {
-    auto minKey = (i * keyRangeSize);
-    auto maxKey = minKey + keyRangeSize - 1;
-    std::thread th(runClientOperations, cache, region, minKey, maxKey,
-                   TX_PER_CLIENT, useSingleHopAndPR);
-    clientThreads.push_back(std::move(th));
-  }
-
-  cluster.getServers()[1].stop();
-
-  for (std::thread& th : clientThreads) {
-    if (th.joinable()) {
-      th.join();
-    }
-  }
-
-  std::ifstream testLog(getClientLogName());
-  std::string fileLine;
-  auto appearances = std::make_shared<std::vector<std::string>>();
-
-  std::regex disconnectRegex(disconnectingStr);
-  if (testLog.is_open()) {
-    while (getline(testLog, fileLine)) {
-      if (std::regex_search(fileLine, disconnectRegex)) {
-        appearances->push_back(fileLine);
-      }
-    }
-  }
-
-  checkAllDisconnectionsAreFromSameServer(appearances);
-
-}  // executeTestCase
-
-TEST(DisconnectEndPointAtException, useSingleHopAndPR) {
-  removeLogFromPreviousExecution();
-  executeTestCase(true);
-}
-
-TEST(DisconnectEndPointAtException, doNotUseSingleHopAndPR) {
-  removeLogFromPreviousExecution();
-  executeTestCase(false);
+TEST(DisconnectEndPointAtException, useOneTransaction) {
+  executeTestCaseOneTransaction();
 }
 
 }  // namespace
